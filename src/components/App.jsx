1a813d5330043b7ab331629cfae88f2ef67946cc
Nodzhak <kostiantyn.nodzhak@gmail.com>
2024-03-20 15:56:34 +0100

    1
    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,94 +1,25 @@
-import React, { useState } from 'react';
-import LoginForm from "./LoginForm";
-import SearchBar from "./SearchBar";
-import LangSwitcher from "./LangSwitcher";
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
 
 const App = () => {
-  const handleLogin = (userData) => {
-    console.log(userData);
-  };
+  const [articles, setArticles] = useState([])
+useEffect(()=>{
+async function fetchArticles(){
+const response = await axios.get(
+  "<https://hn.algolia.com/api/v1/search?query=react>"
+)
+setArticles(response.data.hits)
+}
 
-  const handleSumit = (evt) => {
-  evt.preventDefault();
-  console.log(values);
+fetchArticles();
+},[])
 
-	// Очищаємо форму
-  setValues({
-    login: "",
-    password: "",
-  });
-};
-
-  const [lang, setLang] = useState("uk");
-  const [coffeeSize, setCoffeeSize] = useState("sm");
-  const [hasAccepted, setHasAccepted] = useState(false); 
-
-  const handleSizeChange = (evt) => {
-    setCoffeeSize(evt.target.value);
-  };
-
-  const handleChange = (evt) => {
-    setHasAccepted(evt.target.checked);
-  };
+  
 
   return (
     <div>
-      <div>
-        <h1>Please login to your account!</h1>
-        <LoginForm onSubmit={handleLogin} />
-        <SearchBar />
-      </div>
-      <div>
-        <p>Selected language: {lang}</p>
-        <LangSwitcher value={lang} onSelect={setLang} />
-      </div>
-      <div>
-        <h1>Select coffee size</h1>
-        <label>
-          <input
-            type="radio"
-            name="coffeeSize"
-            value="sm"
-            checked={coffeeSize === "sm"}
-            onChange={handleSizeChange}
-          />
-          Small
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="coffeeSize"
-            value="md"
-            checked={coffeeSize === "md"}
-            onChange={handleSizeChange}
-          />
-          Medium 
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="coffeeSize"
-            value="lg"
-            checked={coffeeSize === "lg"}
-            onChange={handleSizeChange}
-          />
-          Large
-        </label>
-      </div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            name="terms"
-            checked={hasAccepted}
-            onChange={handleChange}
-          />
-          I accept terms and conditions
-        </label>
-        <button type="button" disabled={!hasAccepted}>
-          Proceed
-        </button>
-      </div>
+      <h1>Latest articles</h1>
     </div>
   );
 };
