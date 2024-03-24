9164eb927ad9cd060b80f5b12ae8925d24b398c3
Nodzhak <kostiantyn.nodzhak@gmail.com>
2024-03-24 08:57:38 +0100

    1
    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,24 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
+function App() {
 
-const App = () => {
-  const [articles, setArticles] = useState([])
-useEffect(()=>{
-async function fetchArticles(){
-const response = await axios.get(
-  "<https://hn.algolia.com/api/v1/search?query=react>"
-)
-setArticles(response.data.hits)
+	const arr = [1, 2, 3, 4, 5];
+
+	const res = arr.map((item, id) => {
+		return <p key={id}>{item}</p>;
+	});
+	return (
+		<>
+		{res}
+		</>
+	)
 }
 
-fetchArticles();
-},[])
-
-  
-
-  return (
-    <div>
-      <h1>Latest articles</h1>
-    </div>
-  );
-};
-
 export default App;
+
+
+
+
+
+
+
